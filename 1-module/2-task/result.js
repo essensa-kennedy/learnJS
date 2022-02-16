41890c4e36feaf0969e7ea59b602ef0c8c70b51d
Axl Kennedy <axl.s.kennedy@gmail.com>
2022-02-16 15:41:03 +0300

    1-module 2-task

---
+++ 1-module/2-task/result.js
@@ -0,0 +1,22 @@
+function print(text) {
+	console.log(text);
+ }
+
+function isValid(name) {
+ if (name && !(name.includes(' ')) && name.length >= 4) {
+	 return true
+ } else return false  
+}
+
+function sayHello() {
+	let userName = prompt('Введите ваше имя');
+ 
+	if (isValid(userName)) {
+	  print(`Welcome back, ${userName}!`);
+	} else {
+	  print('Некорректное имя');
+	}
+ }
+ 
+ sayHello();
+ 
